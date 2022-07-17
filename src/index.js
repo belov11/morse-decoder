4812492dbd39be3ff4d1e3982a78d985367eb38f
belov11 <pasha_belov_96@mail.ru>
2022-07-18 00:48:54 +0300

    feat: add code for morse decoder

--- src/index.js
+++ src/index.js
@@ -35,11 +35,31 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '**********' : ' ',
 };
 
 function decode(expr) {
-    // write your solution here
-}
+  let str = [];
+  let res = [];
+  let result =[];
+  for (let i = 0; i < expr.length; i += 2) {
+    res.push((expr[i] + expr[i + 1]));
+  }
+  for (let j = 0; j < res.length; j += 5) {
+    result.push(res.slice(j, j + 5))
+  }
+  result = result.map((item) => item.map((el) => el !== "00" ? (el === "10" ? "." : (el === "11" ? "-" : el)) : ""));
+  result = result.map((item) => item.join(""));
+    for(let elem in result) {
+      for (let key in MORSE_TABLE) {    
+      if (result[elem] === key) {
+       str.push(MORSE_TABLE[key]);
+      } 
+    }
+  }
+  return str.join("");
+  }
+  
 
 module.exports = {
     decode
